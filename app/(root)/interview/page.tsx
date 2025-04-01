4e0df5d587d70bb07982c6119b9d71629444f327
Sushmit Biswas <sushmit.biswas@iiitb.ac.in>
2025-04-01 22:45:49 +0530

    feat: update Agent component to use dynamic user data and enhance call handling logic

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -2,13 +2,15 @@ import Agent from "@/components/Agent";
 import { getCurrentUser } from "@/lib/actions/auth.action";
 
 const Page = async () => {
+  const user = await getCurrentUser();
+
   return (
     <>
       <h3>Interview generation</h3>
 
       <Agent
-        userName= "You"
-        userId="user1"
+        userName={user?.name}
+        userId={user?.id}
         type="generate"
       />
     </>
--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,6 +1,11 @@
+'use client';
+
 import { cn } from '@/lib/utils';
+import { vapi } from '@/lib/vapi.sdk';
 import Image from 'next/image'
-import React from 'react'
+import { useRouter } from 'next/navigation';
+import React, { useEffect } from 'react'
+import { useState } from 'react';
 
 enum CallStatus {
   INACTIVE = "INACTIVE",
@@ -9,14 +14,131 @@ enum CallStatus {
   FINISHED = "FINISHED",
 }
 
-const Agent = ({ userName }: AgentProps) => {
-  const callStatus = CallStatus.FINISHED; // This should be dynamic based on the call status
-  const isSpeaking = true;
-  const messages = [
-    'What is your name?',
-    'My name is John Doe, nice to meet you!',
-  ];
-  const lastMessage = messages[messages.length - 1];
+// 2:45
+interface SavedMessage {
+  role: "user" | "system" | "assistant";
+  content: string;
+}
+
+const Agent = ({ userName, userId, type }: AgentProps) => {
+  const router = useRouter();
+  const [isSpeaking, setIsSpeaking] = useState(false);
+  const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
+  const [messages, setMessages] = useState<SavedMessage[]>([]);
+
+  useEffect(() => {
+    const onCallStart = () => {
+      setCallStatus(CallStatus.ACTIVE);
+    };
+
+    const onCallEnd = () => {
+      setCallStatus(CallStatus.FINISHED);
+    };
+
+    const onMessage = (message: Message) => {
+      if (message.type === "transcript" && message.transcriptType === "final") {
+        const newMessage = { role: message.role, content: message.transcript };
+        setMessages((prev) => [...prev, newMessage]);
+      }
+    };
+
+    const onSpeechStart = () => {
+      console.log("speech start");
+      setIsSpeaking(true);
+    };
+
+    const onSpeechEnd = () => {
+      console.log("speech end");
+      setIsSpeaking(false);
+    };
+
+    const onError = (error: Error) => {
+      console.log("Error:", error);
+    };
+
+    vapi.on("call-start", onCallStart);
+    vapi.on("call-end", onCallEnd);
+    vapi.on("message", onMessage);
+    vapi.on("speech-start", onSpeechStart);
+    vapi.on("speech-end", onSpeechEnd);
+    vapi.on("error", onError);
+
+    return () => {
+      vapi.off("call-start", onCallStart);
+      vapi.off("call-end", onCallEnd);
+      vapi.off("message", onMessage);
+      vapi.off("speech-start", onSpeechStart);
+      vapi.off("speech-end", onSpeechEnd);
+      vapi.off("error", onError);
+    };
+  }, []);
+
+  useEffect(() => {
+    // if (messages.length > 0) {
+    //   setLastMessage(messages[messages.length - 1].content);
+    // }
+
+    // const handleGenerateFeedback = async (messages: SavedMessage[]) => {
+    //   console.log("handleGenerateFeedback");
+
+    //   const { success, feedbackId: id } = await createFeedback({
+    //     interviewId: interviewId!,
+    //     userId: userId!,
+    //     transcript: messages,
+    //     feedbackId,
+    //   });
+
+    //   if (success && id) {
+    //     router.push(`/interview/${interviewId}/feedback`);
+    //   } else {
+    //     console.log("Error saving feedback");
+    //     router.push("/");
+    //   }
+    // };
+
+    if (callStatus === CallStatus.FINISHED) {
+      // if (type === "generate") {
+        router.push("/");
+      // } else {
+        // handleGenerateFeedback(messages);
+      // }
+    }
+  }, [messages, callStatus, type, userId]);
+
+  const handleCall = async () => {
+    setCallStatus(CallStatus.CONNECTING);
+
+    // if (type === "generate") {
+      await vapi.start(process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!, {
+        variableValues: {
+          username: userName,
+          userid: userId,
+        },
+      });
+    // } else {
+    //   let formattedQuestions = "";
+    //   if (questions) {
+    //     formattedQuestions = questions
+    //       .map((question) => `- ${question}`)
+    //       .join("\n");
+    //   }
+
+    //   await vapi.start(interviewer, {
+    //     variableValues: {
+    //       questions: formattedQuestions,
+    //     },
+    //   });
+    // }
+  };
+
+  const handleDisconnect = () => {
+    setCallStatus(CallStatus.FINISHED);
+    vapi.stop();
+  };
+
+  const lastestMessage = messages[messages.length - 1]?.content;
+  const isCallInactiveOrFinished = callStatus === CallStatus.INACTIVE || callStatus === CallStatus.FINISHED;
+
   return (
     <>
       <div className="call-view">
@@ -54,13 +176,13 @@ const Agent = ({ userName }: AgentProps) => {
         <div className="transcript-border">
           <div className="transcript">
             <p
-              key={lastMessage}
+              key={lastestMessage}
               className={cn(
                 "transition-opacity duration-500 opacity-0",
                 "animate-fadeIn opacity-100"
               )}
             >
-              {lastMessage}
+              {lastestMessage}
             </p>
           </div>
         </div>
@@ -77,9 +199,7 @@ const Agent = ({ userName }: AgentProps) => {
             />
 
             <span className="relative">
-              {callStatus === "INACTIVE" || callStatus === "FINISHED"
-                ? "Call"
-                : ". . ."}
+              {isCallInactiveOrFinished ? "Call" : ". . ."}
             </span>
           </button>
         ) : (
