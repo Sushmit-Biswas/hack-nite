2f5ae9977585c2b5542c46ec1302238df09224b2
Sushmit Biswas <sushmit.biswas@iiitb.ac.in>
2025-04-01 17:18:00 +0530

    feat: implement interview details pages with dynamic content

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,18 @@
+import Agent from "@/components/Agent";
+import { getCurrentUser } from "@/lib/actions/auth.action";
+
+const Page = async () => {
+  return (
+    <>
+      <h3>Interview generation</h3>
+
+      <Agent
+        userName= "You"
+        userId="user1"
+        type="generate"
+      />
+    </>
+  );
+};
+
+export default Page;
